b4a3d095aac1e9acc2100a687d15cbeebada4880
MilenaMnatsakanyan <mnatsakanyanmilena87@gmail.com>
2025-08-20 22:31:52 +0400

    main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "account.h"
+/* #include "account.h"
 int main() {
     Account ac1, ac2;
 
@@ -23,4 +23,27 @@ int main() {
     printf("new balance of ac1  : %f\n new ID of ac1 : %d\n", ac1.balance, ac1.ownerId);
     printf("new balance of ac2 %f\n  new ID of ac2 : %d\n", ac2.balance, ac2.ownerId);
     return 0;
+    } */
+#include "person.h"
+int main() {
+    person p1,p2;
+    printf("Enter the age for p1 : ");
+    scanf("%d", &p1.age);
+    printf("Enter the age for p2 : ");
+    scanf("%d", &p2.age);
+    printf("Enter the height for p1 : ");
+    scanf("%f", &p1.height);
+    printf("Enter the age for p2 : ");
+    scanf("%f", &p2.height);
+  int result = older(p1, p2);
+    if(result == 1){
+        printf("first person is older ");
     }
+    else if(result == -1){
+        printf("second person is older ");
+    }
+    else{
+        printf("they have same age ");
+    }
+    return 0;
+}
